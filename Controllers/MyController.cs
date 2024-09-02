e1b9d7185d92880285e39e200487622d73302a8e
OGFTestPipeline <antuxcore@microsoft.com>
2024-09-02 18:06:05 +0530

    new

---
+++ Controllers/MyController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace wa9test.Controllers
+{
+    [ApiController]
+    public class MyController : Controller
+    {
+        [HttpGet]
+        [Route("/checkHeartBeat")]
+        public string PlaceHolder()
+        {
+            return "Heart is beating";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -23,4 +23,6 @@ app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
 
+app.MapControllers();
+
 app.Run();
